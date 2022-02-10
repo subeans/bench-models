52bb16289ce672a4a76073eb8097e3beb448044f
Subin <39542978+subeans@users.noreply.github.com>
2022-02-10 16:38:55 +0900

    update for inceptionv3

--- mxnet/onnx/mx2onnx.py
+++ mxnet/onnx/mx2onnx.py
@@ -2,12 +2,17 @@ import numpy as np
 from mxnet.contrib import onnx as onnx_mxnet
 
 
-def convert(model_name):
-    input_shape=(1,3,224,224)
+def convert(model_name,size):
+    input_shape=(1,3,size,size)
     #convert using onnx
+    target_path = f"./{model_name}"
+    from pathlib import Path
+    Path(target_path).mkdir(parents=True, exist_ok=True)  
+
     onnx_file=f'./{model_name}/{model_name}.onnx'
-    params = f'./{model_name}/model-0000.params'
-    sym=f'./{model_name}/model-symbol.json'
+
+    params = f'../base/{model_name}/model-0000.params'
+    sym=f'../base/{model_name}/model-symbol.json'
     onnx_mxnet.export_model(sym, params, [input_shape], np.float32, onnx_file)
     print('onnx export done')
 
@@ -20,5 +25,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
     model_name = args.model
 
-
-    convert(model_name)
+    img_size=224
+    if model_name == "inception_v3":
+        img_size = 299 
+    convert(model_name,img_size)
