610e159503535d390ce0cd30af32ec4943ff0d16
Subin <39542978+subeans@users.noreply.github.com>
2022-03-14 17:33:41 +0900

    update light models

--- torch/base/export_model.py
+++ torch/base/export_model.py
@@ -4,15 +4,21 @@ import torchvision.models as models
 import numpy as np 
 from pathlib import Path
 
-def download_model(model_name,batchsize,imgsize=224):
+def download_model(model_name,imgsize=224):
     models_detail = {
         'efficientnet_b0' : models.efficientnet_b0(pretrained=True),
         'mobilenet_v2':models.mobilenet_v2(pretrained=True),
+        'resnet18' : models.resnet18(pretrained=True),
+        'mobilenet_v3_small' : models.mobilenet_v3_small(pretrained=True),
+        'mnasnet':models.mnasnet1_0(pretrained=True),
+        'shufflenet' : models.shufflenet_v2_x1_0(pretrained=True),
+        'squeezenet' :models.squeezenet1_0(pretrained=True),
+        
         'inception_v3':models.inception_v3(pretrained=True),
         'resnet50': models.resnet50(pretrained=True),
         'alexnet':models.alexnet(pretrained=True),
         'vgg16':models.vgg16(pretrained=True),
-        'vgg19':models.vgg19(pretrained=True)
+        'vgg19':models.vgg19(pretrained=True),
     }
 
     model = models_detail[model_name]
@@ -22,7 +28,7 @@ def download_model(model_name,batchsize,imgsize=224):
 
     # model(data)
     
-    target_path = f"./{model_name}_{batchsize}/"
+    target_path = f"./{model_name}/"
     from pathlib import Path
     Path(target_path).mkdir(parents=True, exist_ok=True)
 
@@ -36,16 +42,14 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--model',default='resnet50' , type=str)
-    parser.add_argument('--batchsize',default=1 , type=int)
 
 
     args = parser.parse_args()
 
     model_name = args.model
-    batchsize = args.batchsize
     img_size = 224
 
 
     if model_name == 'inception_v3':
         img_size = 299
-    download_model(model_name,batchsize,img_size)
+    download_model(model_name,img_size)
